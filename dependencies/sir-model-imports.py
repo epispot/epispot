74aa45ca6210f7e3b9b460ba4e333f043e4ca4ab
q9i <46249765+quantum9Innovation@users.noreply.github.com>
2020-07-18 13:04:16 -0700

    Added head comment

--- dependencies/sir-model-imports.py
+++ dependencies/sir-model-imports.py
@@ -1,3 +1,10 @@
+"""
+IMPORT: the following in order to run
+ - `sir-model.py`
+     - and all further models
+This is NOT necessary for `\fitters\gradient-descent.py`
+"""
+
 import numpy as np
 from scipy.integrate import odeint
 import matplotlib.pyplot as plt
