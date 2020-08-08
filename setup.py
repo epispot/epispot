caac8bee86331e9558d94eb7daf63993f335205c
quantum9Innovation <46249765+quantum9Innovation@users.noreply.github.com>
2020-08-08 13:32:38 -0700

    New Release Data
    v0.0.2 on PyPi

--- setup.py
+++ setup.py
@@ -5,16 +5,16 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="epispot",
-    version="0.0.1",
+    version="0.0.2",
     author="quantum9innovation",
     description="A tool for modelling infectious diseases.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/quantum9innovation/epi-spot",
+    url="https://quantum9innovation.github.io/epispot",
     packages=setuptools.find_packages(),
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU License",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3',
