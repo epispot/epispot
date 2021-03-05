3efd0768f46a5276ee32cf9c6079ac66eff8f7c0
QLabs <55121845+Quantalabs@users.noreply.github.com>
2021-03-05 14:06:05 -0800

    Update test_critical.py

--- tests/CI/test_critical.py
+++ tests/CI/test_critical.py
@@ -1,4 +1,7 @@
-import os, sys, inspect
+import os
+import sys
+import inspect
+
 currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 repodir = os.path.dirname(os.path.dirname(currentdir))
 sys.path.insert(0, repodir)
