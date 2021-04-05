c70076e2124a71fe4b2753eb8217f4bd2fb4fa1a
q9i <46249765+quantum9Innovation@users.noreply.github.com>
2021-04-05 10:39:03 -0700

    Fixed branch.py

--- scripts/branch.py
+++ scripts/branch.py
@@ -4,17 +4,3 @@ from glob import glob
 
 for fname in os.listdir('./docs/epispot/'):
     os.rename('./docs/epispot/'+fname, './docs/'+fname)
-
-for dname in glob("./*/"):
-    if dname == 'docs' or dname == 'scripts':
-        continue
-    
-    shutil.rmtree(dname)
-
-for fname in os.path.abspath('.'):
-    if fname.endswith('/'):
-        continue
-    if fname == 'scripts/branch.py' or fname.find('docs') != -1:
-        continue
-
-    os.remove(fname)
