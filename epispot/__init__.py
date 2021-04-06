254bdf2678cd54a01cc644138b54ef748cd3ceb9
quantum9innovation <46249765+quantum9Innovation@users.noreply.github.com>
2021-04-06 12:37:44 -0700

    Version Info Changes Preparing for PR

--- epispot/__init__.py
+++ epispot/__init__.py
@@ -20,6 +20,9 @@ from . import plots
 from . import fitters
 
 # version info
-__version__ = "v2.0.1.24"  # version (v#.#.#)
-__stable__ = True  # True --> stable release, False --> unstable release
+__version__ = "v2.1.0.0"  # version (v#.#.#)
+"""epispot's version info"""
+__stable__ = False
+"""stability of the build: True --> stable release (master), False --> unstable release (nightly)"""
 __repo_data_url__ = 'https://www.github.com/epispot/epispot'  # source repo
+"""URL to GitHub source"""
