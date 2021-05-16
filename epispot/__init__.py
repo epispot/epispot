4579cbbbe977e9eae729aea689fc0b4448449430
quantum9innovation <46249765+quantum9Innovation@users.noreply.github.com>
2021-05-16 16:49:56 -0700

    Adds `.. include:: ../README.md` suggestion by @Quantalabs pdoc3 will now use the README to display documentation for epispot but the Python `help()` command will return epispot's inline documentation.

--- epispot/__init__.py
+++ epispot/__init__.py
@@ -1,75 +1,88 @@
 """
-# epispot
+.. include:: ../README.md
+<!--- START: inline documentation
+
+=======
+epispot
+=======
+
 A tool for creating and testing compartmental epidemiological models faster than ever for the mathematical modeling of
 infectious diseases. An idea from https://github.com/henrifroese/infectious_disease_modelling.
+
 ---
-## DOI Information
-[![DOI](https://zenodo.org/badge/280527664.svg)](https://zenodo.org/badge/latestdoi/280527664)
-### Citation:
+
+===============
+DOI Information
+===============
+
+Recommended Citation for Research:
 > q9i, & QLabs. (2021, April 2). epispot/epispot: (Version 2.1.0). Zenodo. http://doi.org/10.5281/zenodo.4624423
 
-## Checking your installation
+==========================
+Checking your installation
+==========================
+
 When using epispot, epispot should automatically run a 'sanity check' to make sure everything was installed correctly.
 To access some of epispot's metadata from the package itself and double-check your installation, run:
-```python
+
 >>> import epispot as epi
 >>> print(epispot.version)
->>> '2.x.x'
+>>> '2.x.x'  # should return something like this
 
 >>> print(epispot.stable)
->>> True
-
->>> print(epispot.source)
->>> 'https://www.github.com/epispot/epispot'
-
->>> print(epispot.docs)
->>> 'https://epispot.github.io/epispot'
-
->>> print(epispot.issues)
->>> 'https://www.github.com/epispot/epispot/issues'
+>>> True  # if this is False, you can always install a stable version from either Anaconda or the main package on PyPI
 
 >>> help(epispot)
 >>> 'Returns this docstring ...'
 
->>> help(epispot.some_module.some_function)
->>> 'Returns function docstring ...'
-```
+===============
+Getting Started
+===============
 
-## Getting Started
 Start with the tutorials in `tests/tutorials`
-(see [GitHub repo](https://www.github.com/epispot/epispot/tree/master/tests/tutorials)). Next, check out the
+(see https://www.github.com/epispot/epispot/tree/master/tests/tutorials). Next, check out the
 documentation at https://epispot.github.io/epispot. You may also find
 it helpful to see the examples in the `tests/examples` folder.
 
-## Release Notes
-See [GitHub repo](https://www.github.com/epispot/epispot/releases) for more info.
+========
+Features
+========
 
-## Features
 - Compartmental models with the following compartments:
     - Susceptible (traditional SIR)
     - Infected (traditional SIR)
     - Recovered (traditional SIR)
-    - Exposed
-    - Dead
-    - Critical
-    - Hospitalized
-- Custom-defined compartmental models
+    - Exposed (extended SIR)
+    - Dead (extended SIR)
+    - Critical (extended SIR)
+    - Hospitalized (extended SIR)
+- Custom-defined compartmental models (useful for research)
 - Graphing and visualization engine
     - Plot model predictions interactively
     - Compare different model predictions
 
-## A Note on Documentation
+===================
+Documentation Notes
+===================
+
 Docstrings provide additional documentation on a certain function.
 They can be accessed via the built-in Python `help()` command.
 These strings are formatted in GitHub-flavored markdown.
-Additionally, all files will have a 'STRUCTURE' label.
+Additionally, all files will have a 'STRUCTURE' label. This README
+is the only exception.
 
-## Credits
+=======
+Credits
+=======
+
 The epispot package is supported by the following contributors:
- - Head of Software & Development: [@quantum9innovation](https://www.github.com/quantum9innovation)
- - Head of Code Maintenance: [@Quantalabs](https://www.github.com/quantalabs)
+ - Head of Software & Development: @quantum9innovation - https://www.github.com/quantum9innovation
+ - Head of Code Maintenance: @Quantalabs - https://www.github.com/quantalabs
 
-Thank you also to all [GitHub contributors](https://www.github.com/epispot/epispot/insights/contributors)
+Thank you also to all GitHub contributors!
+See: https://www.github.com/epispot/epispot/insights/contributors
+
+-->
 """
 
 
