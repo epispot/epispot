b778a673c357caa0ed0c3755a324c8212271cf2d
quantum9innovation <46249765+quantum9Innovation@users.noreply.github.com>
2021-06-21 13:07:12 -0700

    Swap security codes for enhanced version checks Since keeping track of security codes is a pain, and since ensuring that they are not disclosed during workflow runs is extremely difficult, we've decided to swap security codes for enhanced version agreement checks. Essentially, instead of giving the workflow a code as input, you will give it the version tag of the release you wish to publish. This will then be checked against the other version information  obtained in the manifest files. This extra layer of checks ensures  that the person running the workflow is aware of the version which  they intend to publish.
    Changelist:
    - Delete checker.sh (security codes no longer exist)
    - Add docstring to release.py and extra parameter check
    - Change workflows nightly.yml, python-publish.yml to use version tag
    as input and remove security codes
    
    Additional Notes:
    Security (minor):
    The NIGHTLY_SECURITY_CODE and RELEASE_SECURITY_CODE secrets can now be
    deleted.

--- release.py
+++ release.py
@@ -1,5 +1,29 @@
+"""
+This script is intended to check version information agreement before publishing a package.
+
+The script takes two arguments: 
+ (1) Deployment:
+     string equal to either 'stable' or 'nightly'--
+     This indicates which version information to check (setup.py or setup-nightly.py, respectively).
+ (2) Normalized Version Tag:
+     The normalized version tag (i.e. the version tag found in epispot's manifest files--without any 'v' prefixes)--
+     This is intended as a precaution to ensure that whoever triggered the script is aware of the version 
+     that they are deploying.
+
+The script will then run the following routine:
+ (1) Store received version information from parameter
+ (2) Run a query on the corresponding setup file and obtain version information
+ (3) Run a query on epispot's __init__ file and obtain version information
+ (4) Check for agreement:
+     (a) All three sources of version information agree ==>
+         Exit code: 0 (pass)
+     (b) One or more sources of version information are not in agreement ==>
+         Exit code: 1 (fail)
+"""
 
 import sys
+received_version_info = sys.argv[2]
+print('Received version information obtained: '+received_version_info)
 
 
 def nightly():
@@ -51,7 +75,7 @@ for line, _ in enumerate(read_init):
 init_info = read_init[line_no][11:-2]
 print('`epispot/__init__.py` version info found: '+init_info)
 
-if version_info == init_info:
+if received_version_info == version_info and version_info == init_info:
     print('Exited with 0; version information matches')
 else:
     raise ValueError("Exited with 1; version information doesn't match")
