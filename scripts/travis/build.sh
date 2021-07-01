8c9a992cbf6209c0f98922ee959ac12cc716b9fa
Quantalabs <55121845+Quantalabs@users.noreply.github.com>
2021-07-01 15:13:03 -0700

    Debug travis CI

--- scripts/travis/build.sh
+++ scripts/travis/build.sh
@@ -1,6 +1,5 @@
 echo "Installing dependencies..."
 pip install pandas
 pip install matplotlib~=3.4.2
-sudo apt-get install dvipng texlive-latex-extra texlive-fonts-recommended cm-super  # install LaTeX requirements for Travis CI
 pip install -r requirements/requirements-dev.txt
 echo "Finished installing dependencies."
