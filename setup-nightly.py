2adbdacaa19c43dc35ed40dbd8cbd1d3cb061e40
q9i <46249765+quantum9Innovation@users.noreply.github.com>
2021-07-30 17:50:57 -0700

    SECURITY.md Revision 1 This is the first major revision of our SECURITY.md document, and it's designed to meet our organization-wide commitment to better security.
    Changelist:
    - SECURITY.md
        - Include more detailed instructions on the vulnerability
          disclosure process
        - Remove issue creation as a way to expose a vulnerability
        - Add emails and GPG keys to our vulnerability submission options
    - Remove 'known vulnerabilities' badge (cannot be tracked anymore) from
      README-nightly.md
    - setup.py, setup.cfg, setup-nightly.py
        - Add `author_email`
    - Make @Quantalabs a co-owner of the security policy on
      .github/CODEOWNERS

--- setup-nightly.py
+++ setup-nightly.py
@@ -1,34 +1,35 @@
 import setuptools
 
-with open("README-nightly.md", "r") as fh:
+with open('README-nightly.md', 'r') as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="epispot-nightly",
-    version="3.0.0-alpha-2",
-    author="quantum9innovation",
-    description="The nightly version of the epispot package.",
+    name='epispot-nightly',
+    version='3.0.0-alpha-2',
+    author='quantum9innovation',
+    author_email = 'dev.quantum9innovation@gmail.com',
+    description='The nightly version of the epispot package.',
     long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://epispot.github.io",
+    long_description_content_type='text/markdown',
+    url='https://epispot.github.io',
     project_urls={
-        "Repository": "https://github.com/epispot/epispot",
-        "Changelog": "https://github.com/epispot/epispot/tree/master/CHANGELOG.md",
-        "Bug Tracker": "https://github.com/epispot/epispot/issues",
-        "Documentation": "https://epispot.github.io/epispot",
-        "Code Coverage": "https://app.codecov.io/gh/epispot/epispot"
+        'Repository': 'https://github.com/epispot/epispot',
+        'Changelog': 'https://github.com/epispot/epispot/tree/master/CHANGELOG.md',
+        'Bug Tracker': 'https://github.com/epispot/epispot/issues',
+        'Documentation': 'https://epispot.github.io/epispot',
+        'Code Coverage': 'https://app.codecov.io/gh/epispot/epispot'
     },
     packages=setuptools.find_packages('epispot/'),
     scripts=['bin/epispot'],
     classifiers=[
-        "Programming Language :: Python :: 3.7",
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-        "Operating System :: OS Independent",
-        "Development Status :: 5 - Production/Stable",
-        "Intended Audience :: Developers",
-        "Intended Audience :: Healthcare Industry",
-        "Intended Audience :: Science/Research",
-        "Topic :: Scientific/Engineering",
+        'Programming Language :: Python :: 3.7',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Healthcare Industry',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering',
     ],
     python_requires='>=3.7',
     install_requires=[
--- setup.py
+++ setup.py
@@ -1,33 +1,34 @@
 import setuptools
 
-with open("README.md", "r") as fh:
+with open('README.md', 'r') as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="epispot",
-    version="2.1.1",
-    author="quantum9innovation",
-    description="A tool for modeling infectious diseases.",
+    name='epispot',
+    version='2.1.1',
+    author='quantum9innovation',
+    author_email='dev.quantum9innovation@gmail.com',
+    description='A tool for modeling infectious diseases.',
     long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://epispot.github.io",
+    long_description_content_type='text/markdown',
+    url='https://epispot.github.io',
     project_urls={
-        "Repository": "https://github.com/epispot/epispot",
-        "Changelog": "https://github.com/epispot/epispot/tree/master/CHANGELOG.md",
-        "Bug Tracker": "https://github.com/epispot/epispot/issues",
-        "Documentation": "https://epispot.github.io/epispot",
-        "Code Coverage": "https://app.codecov.io/gh/epispot/epispot"
+        'Repository': 'https://github.com/epispot/epispot',
+        'Changelog': 'https://github.com/epispot/epispot/tree/master/CHANGELOG.md',
+        'Bug Tracker': 'https://github.com/epispot/epispot/issues',
+        'Documentation': 'https://epispot.github.io/epispot',
+        'Code Coverage': 'https://app.codecov.io/gh/epispot/epispot'
     },
     packages=setuptools.find_packages('epispot/'),
     classifiers=[
-        "Programming Language :: Python :: 3.7",
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-        "Operating System :: OS Independent",
-        "Development Status :: 5 - Production/Stable",
-        "Intended Audience :: Developers",
-        "Intended Audience :: Healthcare Industry",
-        "Intended Audience :: Science/Research",
-        "Topic :: Scientific/Engineering",
+        'Programming Language :: Python :: 3.7',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Healthcare Industry',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering',
     ],
     python_requires='>=3.6',
     install_requires=['matplotlib', 'numpy', ],
