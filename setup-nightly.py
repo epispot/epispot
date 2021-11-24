50e1259cb0ba83d2b3dd810e30de49dad66ca303
q9i <46249765+quantum9Innovation@users.noreply.github.com>
2021-11-24 11:59:44 -0800

    Python 3.10 (#113)
    * [dep/dev]: Update coverage requirement from ~=5.5 to ~=6.0
    
    Updates the requirements on [coverage](https://github.com/nedbat/coveragepy) to permit the latest version.
    - [Release notes](https://github.com/nedbat/coveragepy/releases)
    - [Changelog](https://github.com/nedbat/coveragepy/blob/master/CHANGES.rst)
    - [Commits](https://github.com/nedbat/coveragepy/compare/coverage-5.5...6.0)
    
    ---
    updated-dependencies:
    - dependency-name: coverage
      dependency-type: direct:development
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Add Python 3.10 compatibility
    This update adds Python 3.10 to epispot's testing suite and makes it
    part of the default environment.
    After this change, Python 3.10 should be used by all GitHub runners and
    will also be our primary focus for testing.
    See the additional notes for update-specific changes.
    
    Changelist:
    - .github/workflows
        - Use Python 3.10 as the default runner environment
        - build.yml
            - Add Python 3.10 to testing matrix
    - requirements/
        - Use absolute latest version of matplotlib to avoid deprecation
          warnings on Python 3.10 (more on this in the additional notes)
    
    Additional Notes:
    
    Deprecation Notice:
    Despite the changes announced in this update, we are *not* deprecating
    any of the previous
    Python versions, including Python 3.7.
    
    Dependency Conflicts:
    Due to the goal of making Python 3.10 the default interpreter for the
    epispot package, we've had to use a release candidate version of
    matplotlib (3.5.0rc1) instead of a stable one.
    This is done to avoid a deprecation warning which occurs because the
    latest stable release still uses distutils, which is deprecated in
    Python 3.10.
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: QLabs <55121845+Quantalabs@users.noreply.github.com>

--- setup-nightly.py
+++ setup-nightly.py
@@ -23,6 +23,9 @@ setuptools.setup(
     scripts=['bin/epispot'],
     classifiers=[
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Operating System :: OS Independent',
         'Development Status :: 5 - Production/Stable',
